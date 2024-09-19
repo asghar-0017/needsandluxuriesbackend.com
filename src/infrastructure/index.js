1a992f3b52b67083706202b3670566d31160fcc8
Sikander-Mirza <sikandersunny2017@gmail.com>
2024-09-19 22:49:25 +0500

    index.js fix

--- src/infrastructure/index.js
+++ src/infrastructure/index.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose")
 
 const ConnectDB = async ()=>{
-  try{
-   await mongoose.connect("mongodb+srv://sikandersunny2017:VWo4sEgFqok4ol3w@cluster0.0g0br.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    console.log("DB connected");
-  }
- 
-  catch(err){
-    console.log("Connection error",err);
-    
-  }
+  const uri = 'mongodb+srv://sikandersunny2017:VWo4sEgFqok4ol3w@cluster0.0g0br.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+  // Connect to MongoDB using Mongoose (without deprecated options)
+  mongoose.connect(uri)
+      .then(() => console.log('Connected to MongoDB'))
+      .catch(err => console.error('Failed to connect to MongoDB', err));
 }
 
 module.exports = {ConnectDB}
