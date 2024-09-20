cda6fd8eeef9ddc8181e9821e89e16bb4da2cf67
Sikander-Mirza <sikandersunny2017@gmail.com>
2024-09-20 20:23:52 +0500

    db connection

--- src/infrastructure/index.js
+++ src/infrastructure/index.js
@@ -4,7 +4,11 @@ const ConnectDB = async ()=>{
   const uri = 'mongodb+srv://sikandersunny2017:VWo4sEgFqok4ol3w@cluster0.0g0br.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
   // Connect to MongoDB using Mongoose (without deprecated options)
-  mongoose.connect(uri)
+  mongoose.connect(uri,{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 20000
+  })
       .then(() => console.log('Connected to MongoDB'))
       .catch(err => console.error('Failed to connect to MongoDB', err));
 }
