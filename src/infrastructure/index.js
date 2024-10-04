ae320ddd93daf864e9d01e089209eb56a764037e
asghar-0017 <asghar.usman0017@gmail.com>
2024-10-05 00:26:12 +0500

    .

--- src/infrastructure/index.js
+++ src/infrastructure/index.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
 require('dotenv').config();
 
-const uri = 'mongodb://mongo:mhhmHPGFAnmJebQVLhGurvdXbvGsAbHH@autorack.proxy.rlwy.net:28421'
+// const uri = 'mongodb://mongo:mhhmHPGFAnmJebQVLhGurvdXbvGsAbHH@autorack.proxy.rlwy.net:28421'
 // const uri = 'mongodb://mongo:fQFaEIQpBGALYhYNpCuCcxcWhhRHCFPi@junction.proxy.rlwy.net:46294'; 
+const uri = "mongodb+srv://fa21bscs7860:PyENalk60fjxwBrf@cluster0.nmg20.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const ConnectDB = async ()=>{
 
   mongoose.connect(uri,{
