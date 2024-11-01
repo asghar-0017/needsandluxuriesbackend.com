3f71b244351107b8ac5e720eba3bc93ce74e3050
asghar-0017 <asghar.usman0017@gmail.com>
2024-11-01 22:30:56 +0500

    .

--- src/Repository/ContactRepo.js
+++ src/Repository/ContactRepo.js
@@ -2,12 +2,11 @@ const ContactModel = require("../model/ContactModel.js");
 
 const SaveContact = async (data) => {
   try {
-    // Save the contact data in the database
     const contact = await ContactModel.create(data);
-    return contact; // return the contact information
+    return contact; 
   } catch (err) {
     console.error('Error saving contact data:', err);
-    throw new Error('Error saving contact data'); // throw the error to be caught in the controller
+    throw new Error('Error saving contact data'); 
   }
 };
 
